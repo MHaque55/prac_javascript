1ae6837ad7ed46faef5896ec8de0db7600f7d1a2
Mohammed Haque <mhaque21@terpmail.umd.edu>
2023-05-23 00:04:18 -0400

    confirm learnt

--- basic/index.js
+++ basic/index.js
@@ -1,6 +1,8 @@
  //First javascript code
 
 let our_panet = "Earth";
-
+let userNavid;
 currentVisitor = prompt("Enter your name please", '');
+userNavid = confirm("Are you Navid?");
+
 alert(`So our hero ${currentVisitor} visiting ${our_panet}`);
